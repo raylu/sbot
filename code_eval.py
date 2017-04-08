84fb8514acb5254e98d9cd40cb62ec32f8aa3041
raylu <lurayl@gmail.com>
2017-04-08 12:39:57 -0700

    add python2, memory cgroup
    copying from
    https://github.com/raylu/pbot/commit/539828eb56d97c549f93dfac887fcd746bdcb61f
    https://github.com/raylu/pbot/commit/309f20a5be4262df8de4567ed1f6570f621ed78f

--- code_eval.py
+++ code_eval.py
@@ -1,9 +1,14 @@
+from os import path
 import subprocess
 
+chroot_dir = path.join(path.dirname(path.abspath(__file__)), 'chroot')
+MB = 1024 * 1024
+
 def nodejs(cmd):
-	args = ['../nsjail/nsjail', '-Mo', '--rlimit_as', '700', '--chroot', 'chroot',
+	args = ['../nsjail/nsjail', '-Mo', '--rlimit_as', '700', '--chroot', chroot_dir,
 			'-R/usr', '-R/lib', '-R/lib64', '--user', 'nobody', '--group', 'nogroup',
-			'--time_limit', '2', '--disable_proc', '--iface_no_lo', '--',
+			'--time_limit', '2', '--disable_proc', '--iface_no_lo',
+			'--cgroup_mem_max', str(50 * MB), '--quiet', '--',
 			'/usr/bin/nodejs', '--print', prep_input(cmd.args)]
 	proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
 			stderr=subprocess.PIPE, universal_newlines=True)
@@ -11,23 +16,27 @@ def nodejs(cmd):
 	if proc.returncode == 0:
 		output = stdout
 	elif proc.returncode == 109:
-		output = 'timed out after 2 seconds'
+		output = 'timed out'
 	else:
 		try:
 			output = stderr.split('\n', 5)[4]
 		except IndexError:
-			output = 'unknown error'
+			if split[0].startswith('FATAL ERROR:'):
+				output = split[0]
+			else:
+				output = 'unknown error'
 	reply(cmd, output)
 
 def irb(cmd):
 	args = ['../nsjail/nsjail', '-Mo', '--chroot', '',
 			'-R/usr', '-R/lib', '-R/lib64', '--user', 'nobody', '--group', 'nogroup',
-			'--time_limit', '2', '--disable_proc', '--iface_no_lo', '--',
+			'--time_limit', '2', '--disable_proc', '--iface_no_lo',
+			'--cgroup_mem_max', str(50 * MB), '--quiet', '--',
 			'/usr/bin/irb', '-f', '--simple-prompt']
 	proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
 	stdout, _ = proc.communicate(prep_input(cmd.args))
 	if proc.returncode == 109:
-		output = 'timed out after 2 seconds'
+		output = 'timed out or memory limit exceeded'
 	else:
 		lines = stdout.split('\n')
 		output = ''
@@ -36,10 +45,34 @@ def irb(cmd):
 				output += line + '\n'
 	reply(cmd, output)
 
-def python3(cmd):
-	args = ['../nsjail/nsjail', '-Mo', '--chroot', 'chroot', '-E', 'LANG=en_US.UTF-8',
+def python2(cmd):
+	args = ['../nsjail/nsjail', '-Mo', '--chroot', chroot_dir, '-E', 'LANG=en_US.UTF-8',
 			'-R/usr', '-R/lib', '-R/lib64', '--user', 'nobody', '--group', 'nogroup',
-			'--time_limit', '2', '--disable_proc', '--iface_no_lo', '--',
+			'--time_limit', '2', '--disable_proc', '--iface_no_lo',
+			'--cgroup_mem_max', str(50 * MB), '--quiet', '--',
+			'/usr/bin/python2', '-ESsi']
+	proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE, universal_newlines=True)
+	stdout, stderr = proc.communicate(prep_input(cmd.args) + '\n')
+	if proc.returncode == 0:
+		if stdout == '':
+			try:
+				output = stderr.split('\n')[-3]
+			except IndexError:
+				output = ''
+		else:
+			output = stdout
+	elif proc.returncode == 109:
+		output = 'timed out or memory limit exceeded'
+	else:
+		output = 'unknown error'
+	reply(cmd, output)
+
+def python3(cmd):
+	args = ['../nsjail/nsjail', '-Mo', '--chroot', chroot_dir, '-E', 'LANG=en_US.UTF-8',
+			'-R/usr', '-R/lib', '-R/lib64', '--user', 'nobody', '--group', 'nogroup',
+			'--time_limit', '2', '--disable_proc', '--iface_no_lo',
+			'--cgroup_mem_max', str(50 * MB), '--quiet', '--',
 			'/usr/bin/python3', '-ISqi']
 	proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
 			stderr=subprocess.PIPE, universal_newlines=True)
@@ -53,7 +86,7 @@ def python3(cmd):
 		else:
 			output = stdout
 	elif proc.returncode == 109:
-		output = 'timed out after 2 seconds'
+		output = 'timed out or memory limit exceeded'
 	else:
 		output = 'unknown error'
 	reply(cmd, output)
