b45976b45690cb1d98db2d85fe7966668371f6bc
Taylor Skidmore <taylor@taylorskidmore.com>
2017-12-14 16:36:02 -0500

    add a weather command and update config example

---
+++ weather.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+import config
+
+rs = requests.Session()
+rs.headers['User-Agent'] = 'sbot (github.com/raylu/sbot)'
+
+def weather(cmd):
+	if not cmd.args:
+		return
+	if not config.bot.weather_key:
+		return
+	response = rs.get('https://api.wunderground.com/api/%s/conditions/q/%s.json' %
+			  (config.bot.weather_key, cmd.args))
+	report = response.json()
+	print(report)
+	if response.status_code == 200 and 'current_observation' in report:
+		report = report['current_observation']
+		cmd.reply('%s: %s, feels like %s. %s. (%s)'
+			  % (report['display_location']['city'],
+			     report['temperature_string'],
+			     report['feelslike_string'],
+			     report['weather'],
+			     report['forecast_url']))
+	elif response.status_code == 200 and 'results' in report['response']:
+		cmd.reply('Got %s results. Try narrowing your search'
+			  % len(report['response']['results']))
+	else:
+		cmd.reply('Error fetching results')
+		
