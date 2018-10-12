bf68364c145824c2531b86ccb39cc24ef5d70edf
raylu <lurayl@gmail.com>
2018-10-12 15:17:02 -0700

    !py[23]: handle missing id on last node's value

--- chroot/run.py
+++ chroot/run.py
@@ -5,7 +5,11 @@ code = sys.stdin.read()
 node = ast.parse(code)
 last_node = node.body[-1]
 if isinstance(last_node, ast.Expr):
-	if not isinstance(last_node.value, ast.Call) or last_node.value.func.id != 'print':
+	last_is_print = False
+	if isinstance(last_node.value, ast.Call):
+		func_name = getattr(last_node.value.func, 'id', None)
+		last_is_print = (func_name == 'print')
+	if not last_is_print:
 		print_expr = ast.Expr(value=ast.Call(func=ast.Name(id='print', ctx=ast.Load()),
 			args=[last_node.value], keywords=[ast.keyword(arg='end', value=ast.Str(s=''))]))
 		node.body[-1] = print_expr
