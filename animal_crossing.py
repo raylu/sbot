48c1c6f75319fd01fa6d99bc8112f9456518b3ec
Taylor Skidmore <taylor@taylorskidmore.com>
2020-04-11 14:53:25 -0400

    address raylu's feedback, change table name to 'user'

--- animal_crossing.py
+++ animal_crossing.py
@@ -4,70 +4,61 @@ import config
 
 if config.bot.acnh_db is not None:
 	db = sqlite3.connect(config.bot.acnh_db)
+	db.row_factory = sqlite3.Row
 
 def friend_code(cmd):
 	if not cmd.args:
-		_fc_list_all(cmd)
+		_user_list_all(cmd)
 		return
 
 	split = cmd.args.split(' ', 1)
 	subcmd = split[0]
-	if subcmd == 'add':
-		_fc_add_or_update(cmd, split)
+	if subcmd == 'set':
+		_user_upsert_friend_code(cmd, split[1])
 	elif subcmd == 'remove':
-		_fc_remove(cmd)
+		_user_remove(cmd)
 	else:
-		_fc_find(cmd, split)
+		_user_find(cmd, split[0])
 
-def _fc_add_or_update(cmd, split):
-	fc = split[1]
+def _user_upsert_friend_code(cmd, code):
 	sender = cmd.sender
-	cur = db.execute('''
-	SELECT id FROM friend_code WHERE id=?
-	''', (sender['id'],))
+	db.execute('''
+	INSERT INTO user VALUES(?, ?, ?)
+	ON CONFLICT(id)
+	DO UPDATE SET code=excluded.code
+	''', (sender['id'], sender['username'], code))
+	db.commit()
+	cmd.reply('Friend code for %s has been set.' % (sender['username']))
 
+def _user_list_all(cmd):
+	cur = db.execute('SELECT username, code FROM user')
 	try:
-		cur.fetchone()
-		db.execute('''
-		UPDATE friend_code SET code=? WHERE id=?
-		''', (fc, sender['id']))
-		db.commit()
-		cmd.reply('Updated friend code!')
-	except:
-		db.execute('''
-		INSERT INTO friend_code VALUES(?, ?, ?)
-		''', (sender['id'], sender['username'], fc))
-		db.commit()
-		cmd.reply('New friend code added!')
-
-def _fc_list_all(cmd):
-	cur = db.execute('SELECT username, code FROM friend_code')
-	try:
-		codes = cur.fetchall()
+		users = cur.fetchall()
 		reply = []
-		for code in codes:
-			reply.append('%s: %s' % (code[0], code[1]))
+		for user in users:
+			reply.append('%s: %s' % (user['username'], user['code']))
 		cmd.reply('\n'.join(reply))
-	except:
+	except sqlite3.OperationalError:
 		cmd.reply('There are no friend codes saved :(')
 
-def _fc_find(cmd, split):
+def _user_find(cmd, user):
 	cur = db.execute('''
-	SELECT username, code FROM friend_code WHERE username LIKE ?
-	''', (split[0],))
+	SELECT username, code FROM user WHERE username LIKE ?
+	''', (user,))
 	try:
-		code = cur.fetchone()
-		cmd.reply('%s: %s' % (code[0], code[1]))
-	except:
-		cmd.reply('Friend code for %s could not be found.' % (split[0]))
+		user = cur.fetchone()
+		cmd.reply('%s: %s' % (user[0], user[1]))
+	except sqlite3.OperationalError:
+		cmd.reply('Friend code for %s could not be found.' % (user))
 
-def _fc_remove(cmd):
+def _user_remove(cmd):
 	db.execute('''
-	DELETE FROM friend_code WHERE id=?
+	DELETE FROM user WHERE id=?
 	''', (cmd.sender['id'],))
 
 	try:
 		db.commit()
-		cmd.reply('Successfully removed your friend code.')
-	except:
-		cmd.reply('Could not remove friend code. Are you sure it exists?')
+		cmd.reply('Successfully removed friend code for %s.' % (cmd.sender['username']))
+	except sqlite3.OperationalError:
+		cmd.reply('Could not remove friend code for %s. Are you sure it exists?' %
+			(cmd.sender['username']))
