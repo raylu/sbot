7d83a382ecc3c4832ee8a257161f8873dc10d0b3
Taylor Skidmore <taylor@taylorskidmore.com>
2020-04-12 16:06:42 -0400

    addressed easy feedback

--- animal_crossing.py
+++ animal_crossing.py
@@ -22,27 +22,34 @@ def stalk_market(cmd):
 
 	split = cmd.args.split(' ', 1)
 	subcmd = split[0]
+	subargs = len(split) == 2 and split[1] or ''
 	if subcmd == 'tz':
-		_stalk_set_timezone(cmd, split)
+		_stalk_set_timezone(cmd, subargs)
 	elif subcmd == 'sell':
-		_stalk_set_sell_price(cmd, split)
+		_stalk_set_sell_price(cmd, subargs)
 	elif subcmd == 'trigger':
-		_stalks_set_sell_trigger(cmd, split)
+		_stalks_set_sell_trigger(cmd, subargs)
+	else:
+		cmd.reply('Unrecognized stalks subcommand %s' % subcmd)
 
-def _stalk_set_sell_price(cmd, split):
-	if len(split) != 2:
+def _stalk_set_sell_price(cmd, price):
+	if not price:
 		_stalk_list_sale_prices(cmd)
 		return
 	user_id = cmd.sender['id']
 	current_time = datetime.now(timezone.utc)
-	value = int(split[1])
+	try:
+		value = int(price)
+	except ValueError:
+		cmd.reply('usage: !stalks sell 123')
+
 	try:
 		with db:
 			db.execute('''
 			INSERT INTO sale_price VALUES (?, ?, ?)
 			''', (user_id, current_time, value))
 		cmd.reply('Sale price recorded!')
-		_stallk_check_sell_triggers(cmd, split[1])
+		_stallk_check_sell_triggers(cmd, price)
 	except sqlite3.IntegrityError:
 		cmd.reply('Could not add sale price. Have you registered a friend code?')
 
@@ -59,13 +66,13 @@ def _stallk_check_sell_triggers(cmd, price):
 		cmd.reply(msg)
 
 def _stalk_list_sale_prices(cmd):
-	# TODO: is there a better way to do this without a gross global?
 	global cached_row_id
 	cur = db.execute('''
 	SELECT sale_price.rowid, *
 	FROM sale_price
 	INNER JOIN user ON sale_price.user_id = user.id
 	WHERE sale_price.rowid > ?
+	ORDER BY sale_price.rowid ASC
 	''', (cached_row_id,))
 
 	prices = cur.fetchall()
@@ -105,9 +112,9 @@ def _stalk_list_sale_prices(cmd):
 		output.append('\n*: user has no timezone record. Store may be closed.')
 	cmd.reply('\n'.join(output))
 
-def _stalks_set_sell_trigger(cmd, split):
-	if len(split) != 2:
-		cmd.reply('usage: !stalks trigger value. Will ping you if someone reports a sale price higher than value.')
+def _stalks_set_sell_trigger(cmd, price):
+	if not price:
+		cmd.reply('usage: !stalks trigger 123\nWill ping you if someone reports a sale price higher than 123.')
 
 	try:
 		with db:
@@ -115,21 +122,21 @@ def _stalks_set_sell_trigger(cmd, split):
 			INSERT INTO sell_trigger VALUES (?, ?)
 			ON CONFLICT(user_id)
 			DO UPDATE SET price=excluded.price
-			''', (cmd.sender['id'], split[1]))
+			''', (cmd.sender['id'], price))
 		cmd.reply('Trigger has been set for %s. You will be pinged if someone reports a price above this.' %
-			(split[1]))
+			(price))
 	except sqlite3.IntegrityError:
 		cmd.reply('Could not insert trigger. Have you registered a friend code?')
 
-def _stalk_set_timezone(cmd, split):
-	if len(split) != 2:
+def _stalk_set_timezone(cmd, tz_name):
+	if not tz_name:
 		cmd.reply('''
 		Specify a timezone from the tz database.
 See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for a complete list.
 		''')
 		return
 
-	tz = dateutil.tz.gettz(split[1])
+	tz = dateutil.tz.gettz(tz_name)
 
 	if tz is None:
 		cmd.reply('Could not find your specified timzone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones')
@@ -139,7 +146,7 @@ See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for a complete 
 	with db:
 		cur = db.execute('''
 		UPDATE user SET timezone=? WHERE id=?
-		''', (split[1], cmd.sender['id']))
+		''', (tz_name, cmd.sender['id']))
 
 	if cur.rowcount:
 		current_time = datetime.now().astimezone(tz)
