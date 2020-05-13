c90167aade84ebfe271c76946c593d941700eb2b
Joshua Gentes <dividinglimits@gmail.com>
2020-05-13 13:46:41 -0400

    code_eval: ignore discord name pings (#16)
    !js ```1+1``` @name#1234

--- tests/test_code_eval.py
+++ tests/test_code_eval.py
@@ -6,10 +6,25 @@ unittest.TestCase.assert_equal = unittest.TestCase.assertEqual
 
 class TestCodeEval(unittest.TestCase):
 	def test_prep_input(self):
+		# basic inputs
 		self.assert_equal(code_eval.prep_input('1+2*3/4'), '1+2*3/4')
+
+		# code inputs
 		self.assert_equal(code_eval.prep_input('`1+2`'), '1+2')
 		self.assert_equal(code_eval.prep_input('```3/4```'), '3/4')
 		self.assert_equal(code_eval.prep_input('```\n1+2\n3/4\n```'), '1+2\n3/4')
 		self.assert_equal(code_eval.prep_input('```1+2\n3/4\n```'), '1+2\n3/4')
-		self.assert_equal(code_eval.prep_input('```py\n1+2\n3/4\n```'), '1+2\n3/4\n')
+		self.assert_equal(code_eval.prep_input('```py\n1+2\n3/4\n```'), '1+2\n3/4')
 		self.assert_equal(code_eval.prep_input('```\n1+2'), '1+2')
+
+		# code inputs with ping
+		self.assert_equal(code_eval.prep_input('```3/4``` @name#123'), '3/4')
+		self.assert_equal(code_eval.prep_input('```py 3/4``` @name#123'), 'py 3/4')
+		self.assert_equal(code_eval.prep_input('```py\n3/4``` @name#123'), '3/4')
+		self.assert_equal(code_eval.prep_input('```py\n3/4```\n@name#123'), '3/4')
+
+		# invalid code input
+		self.assert_equal(code_eval.prep_input('`1+2` @name#123'), '1+2` @name#123')
+		self.assert_equal(code_eval.prep_input('```\n1+2```\n2+3'), '1+2')
+		self.assert_equal(code_eval.prep_input('```\n1+2```\n2+3\n``` @name#123'), '1+2')
+		self.assert_equal(code_eval.prep_input('```\n1+2```\n```\n@name#123'), '1+2')
