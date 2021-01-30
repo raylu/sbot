raylu/sbot

commit 69cde5fb15cdc0b80ffa7ce59e82c4449bc6112a

instagram: don't save last_timestamp unless it's newer
the instagram API sometimes returns incomplete media,
causing the bot to re-post

instagram.py
@@ -27,8 +27,11 @@ def new_media(bot):
 					continue
 				post_media(bot, insta_conf['channels'], media)
 				time.sleep(2)
-		config.state.instagram[insta_conf['user_id']] = data[-1]['timestamp']
-		config.state.save()
+
+		new_last_timestamp = data[-1]['timestamp']
+		if new_last_timestamp > last_timestamp:
+			config.state.instagram[insta_conf['user_id']] = new_last_timestamp
+			config.state.save()
 
 def post_media(bot, channel_ids, media):
 	embed = {

commit 91ffa320a65c8c65edf1413101d538d9a2150db6

instagram: fix saving last_timestamp of new accounts
broken in 69cde5f

instagram.py
@@ -29,7 +29,7 @@ def new_media(bot):
 				time.sleep(2)
 
 		new_last_timestamp = data[-1]['timestamp']
-		if new_last_timestamp > last_timestamp:
+		if last_timestamp is None or new_last_timestamp > last_timestamp:
 			config.state.instagram[insta_conf['user_id']] = new_last_timestamp
 			config.state.save()
 