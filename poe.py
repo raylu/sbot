raylu/sbot

commit b56b7ace1742c6793f77fadd6e59acff0c482770

poe: show icons for currency

poe.py
@@ -105,15 +105,17 @@ def _search(league, q):
 		name = line.get('name')
 		if name is None:
 			name = line['currencyTypeName']
+			(detail,) = (d for d in data['currencyDetails'] if d['name'] == name)
+			line['icon'] = detail['icon']
 		if q in name.casefold():
 			names.add(name)
 			matches.append(line)
 			if q == name.casefold():
 				exact = True
+				break
 
 	if exact:
-		matches = [match for match in matches
-				if match.get('name', match.get('currencyTypeName')).casefold() == q]
+		matches = [matches[-1]]
 		names = {matches[0].get('name', matches[0].get('currencyTypeName'))}
 	return names, matches
 

commit fdaeea664659788ed3b55506ec4db5cded4c3251

poe: re-fix 5L/6L exact matches
broken in b56b7ace1742c6793f77fadd6e59acff0c482770

poe.py
@@ -112,10 +112,12 @@ def _search(league, q):
 			matches.append(line)
 			if q == name.casefold():
 				exact = True
-				break
+				# there may be other exact matches (5L/6L), so we cannot break
 
 	if exact:
-		matches = [matches[-1]]
+		# there may be multiple lines that are exact matches (5L/6L)
+		matches = [match for match in matches
+				if match.get('name', match.get('currencyTypeName')).casefold() == q]
 		names = {matches[0].get('name', matches[0].get('currencyTypeName'))}
 	return names, matches
 