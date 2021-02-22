eab6c56a2a32d38c9e0c85838fcc361560b8fe33
raylu <90059+raylu@users.noreply.github.com>
2021-02-22 09:04:27 -0800

    poe: add multi match unittest

--- tests/test_poe.py
+++ tests/test_poe.py
@@ -22,11 +22,15 @@ class TestPoe(unittest.TestCase):
 	def tearDownClass(cls):
 		poe.rs = cls.original_rs
 
-	def assert_reply(self, q, value):
+	def assert_reply(self, q, value=None, matches=None):
 		cmd = MockCmd(q)
 		poe.price(cmd)
-		self.assert_equal(cmd.reply_text, '')
-		self.assert_equal(cmd.reply_embed['fields'][0]['value'], textwrap.dedent(value).strip())
+		if value is not None:
+			self.assert_equal(cmd.reply_text, '')
+			self.assert_equal(cmd.reply_embed['fields'][0]['value'], textwrap.dedent(value).strip())
+		else:
+			self.assert_equal(cmd.reply_embed, None)
+			self.assert_equal(set(cmd.reply_text.split(', ')), set(matches))
 
 	def test_price(self):
 		self.assert_reply('the strat', 'The Strategist: 45.0 chaos')
@@ -34,14 +38,25 @@ class TestPoe(unittest.TestCase):
 
 	def test_exact(self):
 		self.assert_reply('enlighten support', '''
-		Enlighten Support (level 4) (20%) (corrupted): 390.9 chaos, 3.9 exalted
-		Enlighten Support (level 3): 59.0 chaos
-		Enlighten Support (level 2): 27.6 chaos
-		Enlighten Support (level 1): 22.0 chaos
-		Enlighten Support (level 3) (corrupted): 15.0 chaos
-		Enlighten Support (level 1) (corrupted): 15.0 chaos
-		Enlighten Support (level 2) (corrupted): 14.0 chaos
+			Enlighten Support (level 4) (20%) (corrupted): 390.9 chaos, 3.9 exalted
+			Enlighten Support (level 3): 59.0 chaos
+			Enlighten Support (level 2): 27.6 chaos
+			Enlighten Support (level 1): 22.0 chaos
+			Enlighten Support (level 3) (corrupted): 15.0 chaos
+			Enlighten Support (level 1) (corrupted): 15.0 chaos
+			Enlighten Support (level 2) (corrupted): 14.0 chaos
 		''')
+
+	def test_multi_match(self):
+		self.assert_reply('exalt', matches=[
+			"Redeemer's Exalted Orb",
+			"Warlord's Exalted Orb",
+			"Crusader's Exalted Orb",
+			"Hunter's Exalted Orb",
+			"Exalted Orb",
+			"Exalted Shard",
+		])
+		self.assert_reply('promesse d', matches=["promesse d'atziri", 'la promesse du lapidaire'])
 
 def get(url, params=None):
 	if url == 'https://poe.ninja/':
@@ -52,7 +67,14 @@ def get(url, params=None):
 			data = json.load(f)
 		return mock.Mock(json=mock.Mock(return_value=data))
 	elif url == 'https://poe.ninja/api/data/itemoverview':
+		if params['type'] == 'Currency':
+			return mock.Mock(status_code=404)
 		filename = 'itemoverview_%s_%s.json' % (params['league'].lower(), params['type'].lower())
+		with open(path.join(fixtures_dir, filename)) as f:
+			data = json.load(f)
+		return mock.Mock(status_code=200, json=mock.Mock(return_value=data))
+	elif url == 'https://poe.ninja/api/data/currencyoverview':
+		filename = 'currencyoverview_%s_%s.json' % (params['league'].lower(), params['type'].lower())
 		with open(path.join(fixtures_dir, filename)) as f:
 			data = json.load(f)
 		return mock.Mock(status_code=200, json=mock.Mock(return_value=data))
@@ -62,6 +84,7 @@ def get(url, params=None):
 class MockCmd:
 	def __init__(self, args):
 		self.args = args
+		self.reply_text = self.reply_embed = None
 
 	def reply(self, text, embed=None):
 		self.reply_text = text
