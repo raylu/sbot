63f66bb3e1c03f700948ca6a9d92e8d2fec450c6
raylu <90059+raylu@users.noreply.github.com>
2022-11-04 22:16:46 -0700

    sbds: fix enemey buff icons, refactor into get_embed

--- sbds.py
+++ sbds.py
@@ -42,43 +42,44 @@ class SBDS:
 def sbds(cmd: 'bot.CommandEvent'):
 	if not cmd.args:
 		return
-	query = cmd.args.casefold()
+	name = cmd.args.casefold()
+	embed = get_embed(name)
+	if embed is None:
+		cmd.reply(f'couldn\'t find "{name}"')
+	else:
+		cmd.reply('', embed)
 
+def get_embed(name: str) -> dict:
 	data = _get_data()
 	for spell_id, spell in itertools.chain(data.spells['SPELL'].items(), data.spells['EVOLVED'].items()):
 		if spell['spellName'] is None:
 			continue
-		if lang := data.matches_key(query, spell['spellName']):
-			cmd.reply('', {
+		if lang := data.matches_key(name, spell['spellName']):
+			return {
 				'title': data.translate(spell['spellName'], lang),
 				'description': data.translate(spell['levelUpDescriptions'][0], lang),
 				'thumbnail': {'url': f'https://sbds.fly.dev/static/data/spells/{spell_id}.png'}
-			})
-			return
+			}
 	for aura_pair in data.spells['AURA']:
 		for aura in aura_pair:
-			if lang := data.matches_key(query, aura['titleText']):
-				cmd.reply('', {
+			if lang := data.matches_key(name, aura['titleText']):
+				return {
 					'title': data.translate(aura['titleText'], lang),
 					'description': data.translate_all(aura['description'], lang),
 					'thumbnail': {'url': f'https://sbds.fly.dev/static/data/spells/{aura["titleText"]}.png'}
-				})
-				return
-	for buff_pair in data.buffs:
-		for buff in buff_pair:
+				}
+	for player_buff, enemy_buff in data.buffs:
+		for buff in (player_buff, enemy_buff):
 			if not buff:
 				continue
-			if lang := data.matches_key(query, buff['shrineText']):
+			if lang := data.matches_key(name, buff['shrineText']):
 				embed = {
 					'title': data.translate(buff['shrineText'], lang),
-					'thumbnail': {'url': f'https://sbds.fly.dev/static/data/buffs/{buff["shrineText"]}.png'}
+					'thumbnail': {'url': f'https://sbds.fly.dev/static/data/buffs/{player_buff["shrineText"]}.png'}
 				}
 				if 'notificationText' in buff:
 					embed['description'] = data.translate(buff['notificationText'], lang)
-				cmd.reply('', embed)
-				return
-
-	cmd.reply(f'couldn\'t find "{query}"')
+				return embed
 
 _cache = None
 def _get_data():
